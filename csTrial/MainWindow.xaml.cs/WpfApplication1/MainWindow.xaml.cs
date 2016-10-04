48b7e0bdf0f2ff54164558bae0791a504f4a7fba
Dan Zabinski <dzabinski@peerless-av.com>
2016-10-03 20:15:56 -0500

    Added Exit button and logic to get date.

--- csTrial/MainWindow.xaml.cs/WpfApplication1/MainWindow.xaml.cs
+++ csTrial/MainWindow.xaml.cs/WpfApplication1/MainWindow.xaml.cs
@@ -29,5 +29,28 @@ namespace WpfApplication1
         {
             MessageBox.Show("The First App is in the book!");
         }
+       /* private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }*/
+
+        private void Exit_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Another button in the book!");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Another button in the book!");
+            Environment.Exit(0);
+        }
+
+        private void HelloWorld_Click(object sender, RoutedEventArgs e)
+        {
+            //need to insert command to ask for name
+            DateTime localDate = DateTime.Now;
+            string HWorld = localDate.ToString("yyyymmdd");
+            MessageBox.Show(HWorld);
+        }
     }
 }
